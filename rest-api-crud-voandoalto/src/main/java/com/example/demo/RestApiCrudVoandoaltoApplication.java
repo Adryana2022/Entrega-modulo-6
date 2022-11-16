916c401a42a0dee5d0686e5b50c9666d643b0fcf
Adriana Albuquerque de Almeida <98193318+Adryana2022@users.noreply.github.com>
2022-11-16 13:49:04 -0300

    Primeiro Commit

---
+++ rest-api-crud-voandoalto/src/main/java/com/example/demo/RestApiCrudVoandoaltoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCrudVoandoaltoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCrudVoandoaltoApplication.class, args);
+	}
+
+}
---
+++ rest-api-crud-voandoalto/src/main/java/com/example/demo/Usuario.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	long id;
+	String nome;
+	long cpf;
+	String destino;
+
+}
